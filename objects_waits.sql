f48162fc43ae5cfb1d58ed2ae0abf7a8ac1f8d1b
Baki Sahin <bakisahin@yahoo.com>
2018-04-18 07:37:18 +0300

    Update objects_waits.sql

--- objects_waits.sql
+++ objects_waits.sql
@@ -15,3 +15,38 @@ and active_session_history.current_obj# = dba_objects.object_id
  group by dba_objects.object_name, dba_objects.object_type, active_session_history.event
  order by 4 desc)
 where rownum < 6;
+
+--SQL with the highest I/O in the past one day
+select * from 
+(
+SELECT /*+LEADING(x h) USE_NL(h)*/ 
+       h.sql_id
+,      SUM(10) ash_secs
+FROM   dba_hist_snapshot x
+,      dba_hist_active_sess_history h
+WHERE   x.begin_interval_time > sysdate -1
+AND    h.SNAP_id = X.SNAP_id
+AND    h.dbid = x.dbid
+AND    h.instance_number = x.instance_number
+AND    h.event in  ('db file sequential read','db file scattered read')
+GROUP BY h.sql_id
+ORDER BY ash_secs desc )
+where rownum
+
+--Top CPU consuming queries since past one day
+select * from (
+select 
+	SQL_ID, 
+	sum(CPU_TIME_DELTA), 
+	sum(DISK_READS_DELTA),
+	count(*)
+from 
+	DBA_HIST_SQLSTAT a, dba_hist_snapshot s
+where
+ s.snap_id = a.snap_id
+ and s.begin_interval_time > sysdate -1
+	group by 
+	SQL_ID
+order by 
+	sum(CPU_TIME_DELTA) desc)
+where rownum
